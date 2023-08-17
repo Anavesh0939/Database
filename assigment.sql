1a3765b095a9ac7ed5a7ec95b439e7a432bb723b
Anavesh vishwakarma <115550669+Anavesh0939@users.noreply.github.com>
2023-08-17 14:55:26 +0530

    Add files via upload

---
+++ assigment.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 17, 2023 at 11:23 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assigment`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+-- Error reading structure for table assigment.customer: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`customer`
+-- Error reading data for table assigment.customer: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`customer`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Employee`
+--
+-- Error reading structure for table assigment.Employee: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`Employee`
+-- Error reading data for table assigment.Employee: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`Employee`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `examdb`
+--
+-- Error reading structure for table assigment.examdb: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`examdb`
+-- Error reading data for table assigment.examdb: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`examdb`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Incentive`
+--
+-- Error reading structure for table assigment.Incentive: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`Incentive`
+-- Error reading data for table assigment.Incentive: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`Incentive`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Incentiveinfo`
+--
+-- Error reading structure for table assigment.Incentiveinfo: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`Incentiveinfo`
+-- Error reading data for table assigment.Incentiveinfo: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`Incentiveinfo`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Information`
+--
+-- Error reading structure for table assigment.Information: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`Information`
+-- Error reading data for table assigment.Information: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`Information`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salesperson`
+--
+-- Error reading structure for table assigment.salesperson: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`salesperson`
+-- Error reading data for table assigment.salesperson: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`salesperson`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stud`
+--
+-- Error reading structure for table assigment.stud: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`stud`
+-- Error reading data for table assigment.stud: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`stud`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Student`
+--
+-- Error reading structure for table assigment.Student: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `assigment`.`Student`
+-- Error reading data for table assigment.Student: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `assigment`.`Student`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
